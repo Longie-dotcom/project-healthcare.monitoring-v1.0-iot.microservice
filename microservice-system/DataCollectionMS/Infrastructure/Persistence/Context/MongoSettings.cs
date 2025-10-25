9499ae659d5fad773e86790da22c6667618bf835
Long Dong <longdong32120@gmail.com>
2025-10-25 16:19:54 +0700

    delete all current and init new

--- microservice-system/DataCollectionMS/Infrastructure/Persistence/Context/MongoSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Infrastructure.Persistence.Context
-{
-
-    public class MongoSettings
-    {
-        public string ConnectionString { get; set; } = "";
-        public string DatabaseName { get; set; } = "";
-    }
-}
